48672a35f251bba2168b85534ed8d646579b7e18
Ian Nduta <gathonjiandutai@gmail.com>
2024-06-09 19:48:51 -0400

    update

--- JasonPlaceHolder/scripts/user.js
+++ JasonPlaceHolder/scripts/user.js
@@ -17,11 +17,30 @@ function populateTable(){
         console.log(data);
 
         for(let i = 0; i < data.length; i++){
-            let row = table.insertRow(-1);
-            let cell1 = row.insertCell(0);
-            let cell2 = row.insertCell(1);
-            cell1.innerHTML = data[i].name;
-            cell2.innerHTML = data[i].username;
+            creatingTheTable(data[i])
         }
     })
+}
+
+function creatingTheTable(apiArray){
+    const nationalParkTable = document.getElementById("nationalParkTable");
+    let row = nationalParkTable.insertRow(-1);
+
+    let cell1 = row.insertCell(0);
+    cell1.innerHTML = apiArray.name;
+
+    let cell2 = row.insertCell(1);
+    cell2.innerHTML = apiArray.username;
+
+    let cell3 = row.insertCell(2);
+    cell3.innerHTML = apiArray.email;
+
+    let cell4 = row.insertCell(3);
+    cell4.innerHTML = apiArray.phone;
+
+    let cell5 = row.insertCell(4);
+    cell5.innerHTML = apiArray.website;
+
+    let cell6 = row.insertCell(5);
+    cell6.innerHTML = apiArray.username;
 }
--- demos/webiste1/scripts/index.js
+++ demos/webiste1/scripts/index.js
@@ -1,35 +1,38 @@
-"use stict";
+"use strict";
 
 const hello = document.getElementById("hello");
-const apiUrlListCategories = https
-
+const apiUrlListCategories = "https://www.themealdb.com/api/json/v1/1/categories.php"
+const apiUrlMealsInCategoryBase = "https://www.themealdb.com/api/json/v1/1/filter.php?c="
 
 const categoriesRow = document.getElementById("categoriesRow")
 
 window.onload = function(){
-    
+   // hello.innerHTML = "Hello Class";
 
 
     fetch(apiUrlListCategories)
-    .then(response => response.json)
-    .then(data => {
+    .then( response => response.json())
+    .then( data => {
         console.log(data);
-        for(let category of data.categories)
-            createCategoryDiv(category)
-    })
+        data.categories.forEach(createCategoryDiv);
+       
+    });
+
+
 }
 
 
 
 function createCategoryDiv(category){
-    let div = document.createElement("div")
+    let div = document.createElement("div");
     div.className = "col";
     categoriesRow.appendChild(div);
 
     let a = document.createElement("a");
-    a.href = "mealsInCategory.html?category="+ category.strCategory;
+    a.href = "mealsInCategory.html?category=" + category.strCategory;
     a.innerHTML = category.strCategory;
 
     div.appendChild(a);
+
     console.log(category.strCategory);
 }
